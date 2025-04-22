a41ae1c84e0b2d4d42ec122769503293fe07f199
Owen Halliday <hallidayo@moravian.edu>
2025-04-22 18:35:30 -0400

    check for dev with AWS_SAM_LOCAL env var

--- utils/sql.py
+++ utils/sql.py
@@ -2,9 +2,19 @@ import psycopg
 import os
 from queries.utils.secrets_manager import get_secret
 
+
 def connect():
-    secret_name = os.environ.get('DB_SECRET_NAME')
-    secret = get_secret(secret_name)
+    if os.getenv("AWS_SAM_LOCAL", ""):
+        secret = {
+            "db": os.environ.get("POSTGRES_DB"),
+            "username": os.environ.get("POSTGRES_USER"),
+            "password": os.environ.get("POSTGRES_PASSWORD"),
+            "host": os.environ.get("POSTGRES_HOST"),
+            "port": os.environ.get("POSTGRES_PORT"),
+        }
+    else:
+        secret_name = os.environ.get("DB_SECRET_NAME")
+        secret = get_secret(secret_name)
 
     conn_params = {
         "dbname": secret['db'],
